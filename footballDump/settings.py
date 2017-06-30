564539a993e80cff55019c93d863ea680581454f
Ari Boyarsky <aboyarsky@uchicago.edu>
2017-06-29 21:59:47 -0400

    email change

--- footballDump/settings.py
+++ footballDump/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'fbStore.apps.fbStoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
